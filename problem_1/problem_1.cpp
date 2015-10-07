150aeaeabae8dde768c3d025df75a76489035ea0
B Moore <malide@opensource.osu.edu>
2015-10-07 13:12:01 -0400

    Problem 1 and initial gitignore
    Added a C++ version of problem 1. Could be made more efficient slightly.

---
+++ problem_1/problem_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int place = 3;
+	int place2 = 5;
+	int total = 0;
+	while(place<1000||place2<1000){
+		total += place;
+		place+=3;
+		if(place2<1000){
+			total+=place2;
+			place2+=5;
+		}
+	}
+	cout << total << endl;
+	return 0;
+}
